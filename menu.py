44b9ca8d1e35e7d80a637981c699b4c5b9bedd2c
KlareoN <justindima2016@yandex.ru>
2021-07-17 21:05:17 +0300

    #1

---
+++ menu.py
@@ -0,0 +1,15 @@
+import imgui
+
+def menu():
+
+    imgui.set_next_window_size(250, 250)
+ #   imgui.set_next_window_position(0, 0)
+    imgui.begin(
+        "Hello World",
+       # flags=imgui.WINDOW_NO_MOVE |
+       #       imgui.WINDOW_NO_RESIZE |
+       #       imgui.WINDOW_NO_SAVED_SETTINGS
+    )
+    imgui.text("Test")
+
+    imgui.end()
